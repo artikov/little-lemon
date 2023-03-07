6644435ea39c03f01604f523838ee2a6c1257816
Oybek Artikov <oybekartikov@icloud.com>
2023-03-07 17:30:48 +0500

    Remove Header component & Style Specials

--- src/components/Special.jsx
+++ src/components/Special.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Special = () => {
-  return <div>Special</div>;
+  return (
+    <div className="special__card">
+      <div className="special__img">
+        <img src="/icons_assets/greek_salad.jpg" alt="" />
+      </div>
+      <div className="special__info">
+        <h3 className="special__title">Greek Salad</h3>
+        <p className="special__subtitle">Greek Salad</p>
+        <p className="special__price">$ 12.00</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+          inventore quaerat officia dolore. Iusto nemo ex perferendis quisquam,
+          quia perspiciatis.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Special;
--- src/components/Specials.jsx
+++ src/components/Specials.jsx
@@ -4,10 +4,15 @@ import Special from "./Special";
 const Specials = () => {
   return (
     <div className="specials__container">
-      Specials
-      <Special />
-      <Special />
-      <Special />
+      <div className="specials__order">
+        <p className="main__subtitle">Specials</p>
+        <button className="order__button">Order Online</button>
+      </div>
+      <div className="specials-food__container">
+        <Special />
+        <Special />
+        <Special />
+      </div>
     </div>
   );
 };
