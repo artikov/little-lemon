ba4de53eb976d6bebbf0d955b4e4994e6485e3e6
Oybek Artikov <oybekartikov@icloud.com>
2023-03-08 00:31:30 +0500

    Update About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,19 @@ import React from "react";
 const About = () => {
   return (
     <section id="about" className="about__container">
-      About
+      <div className="about__info">
+        <p className="main__subtitle">Little Lemon</p>
+        <p>Chicago</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+          mollitia architecto aliquam tenetur veritatis accusamus voluptatum
+          reiciendis error recusandae ut.
+        </p>
+      </div>
+      <div className="about__img">
+        <img src="/icons_assets/Mario_and_Adrian_A.jpg" alt="" />
+        <img src="/icons_assets/Mario_and_Adrian_b.jpg" alt="" />
+      </div>
     </section>
   );
 };
